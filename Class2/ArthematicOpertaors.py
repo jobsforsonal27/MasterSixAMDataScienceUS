ddf22421bfe5f7c0579055a6021f59f1eb6e9c6c
unknown <bsuraj23@gmail.com>
2026-02-26 07:13:38 +0530

    adding 2 folders class1 and class2

---
+++ Class2/ArthematicOpertaors.py
@@ -0,0 +1,29 @@
+# Arthematic operators are + - / %
+
+a = 2
+b = 2
+c = a + b
+
+d = a - b
+
+value1 = 1
+value2 = 2
+
+result = a + b
+print("a+b", result)
+
+result = a - b
+print("a-b", result)
+
+result = a * b
+print("a*b", result)
+
+result = a**b
+print("a**b", result)
+
+result = a % b
+print("a%b", result)
+print(
+    "The % symbol in Python is called the Modulo Operator. It returns the remainder of dividing the left hand operand by right hand operand"
+)
+print("Whats does // does ???????/")
---
+++ Class2/StringTopic.py
@@ -0,0 +1,16 @@
+# Strings are Data Types in Python
+
+# str1 = "Narender"
+# str2 = "\nModi"
+
+# print(str1 + "" + str2)
+
+
+
+# print("Have you observed syntax for str3\t" + str3)
+str3 = """otherName
+ I am other line """
+
+
+justVar = str3[9]
+print(justVar)
